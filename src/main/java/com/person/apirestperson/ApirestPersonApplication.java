52145a3ffda72b9f29674273525ae7a58d959f90
tassio <tassiokp206@gmail.com>
2021-07-28 02:33:40 -0300

    removendo anotacoes @EntityScan da classe principal

--- src/main/java/com/person/apirestperson/ApirestPersonApplication.java
+++ src/main/java/com/person/apirestperson/ApirestPersonApplication.java
@@ -5,13 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.person.apirestperson")
-@EntityScan("com.person.apirestperson")
-@EnableJpaRepositories("com.person.apirestperson")
+//@SpringBootApplication(scanBasePackages = "com.person.apirestperson")
+//@EntityScan("com.person.apirestperson")
+//@EnableJpaRepositories("com.person.apirestperson")
+@SpringBootApplication
 public class ApirestPersonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApirestPersonApplication.class, args);
 	}
-
 }
